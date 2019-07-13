391f4e04da75b6091cadc0c630251f51792d993e
tebanwolox <esteban.bedoya@wolox.co>
2019-07-12 22:14:55 -0500

    first commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,35 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  foto: any;
 
+  constructor(private camera: Camera) {}
+
+  capturarFoto() {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }).then(imageData => {
+      this.foto = `data:image/jpeg;base64,${imageData}`;
+    });
+  }
+
+  buscarFoto() {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }).then(imageData => {
+      this.foto = `data:image/jpeg;base64,${imageData}`;
+    });
+  }
 }
